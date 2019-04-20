e60989b3071c58b2c4a8454051f6b06300c5c0dc
Allen Francis <lallenfrancisl@gmail.com>
2019-04-20 22:02:57 +0530

    add page reveal animation for borrow option in the homepage

--- staticfiles/component/assets/js/main.js
+++ staticfiles/component/assets/js/main.js
@@ -18,11 +18,79 @@ temporaryNode.innerHTML = `<div class="component">
 </div>`;
 
 let nodeToInsert = null;
-newComponentButton.addEventListener('click', function() {
+newComponentButton.addEventListener('click', function () {
     nodeToInsert = temporaryNode.cloneNode(true);
     borrow_form.insertBefore(nodeToInsert, newComponentButton);
     console.log('clicked button');
 });
 
+// let borrowPageTransition = anime({
+//     targets: '.underline',
+//     // width: ['50%', '100%'],
+//     left: '+=50%',
+//     easing: 'linear',
+//     autoplay: false
+// });
 
+let menuButtons = document.getElementsByClassName('menu-item');
+let header = document.getElementsByTagName('header')[0];
 
+let borrowButton = menuButtons[0];
+let returnButton = menuButtons[1];
+
+let borrowPageTransition = anime.timeline({
+    autoplay: false,
+    easing: 'linear'
+});
+
+borrowPageTransition
+.add({
+    targets: '.underline',
+    left: '50%',
+    duration: 200,
+    complete: function() {
+        console.log('first step complete')
+    }
+})
+.add({
+    targets: returnButton,
+    opacity: 0,
+    translateX: '60vw',
+    duration: 500
+})
+.add({
+    targets: '.menu',
+    // transateX: '500px',
+    translateY: '-100vh',
+    duration: 200,
+    // width: '25%',
+    complete: function() {
+        console.log('third step complete');
+    },
+    begin: function() {
+        console.log('third step begin');
+    }
+})
+.add({
+    targets: '.borrow-form',
+    translateY: '-108vh',
+    duration: 200
+});
+
+borrowButton.addEventListener('click', function () {
+    console.log('clicked borrow button');
+    borrowPageTransition.play();
+    borrowPageTransition.finished.then(function() {
+        borrowPageTransition.reverse();
+        console.log('animation reversed');
+    })
+});
+
+document.getElementsByClassName('back-to-home')[0].addEventListener('click', function() {
+    // borrowPageTransition.reverse();
+    borrowPageTransition.play();
+    borrowPageTransition.finished.then(function() {
+        borrowPageTransition.reverse();
+        console.log('animation reversed');
+    })
+});
