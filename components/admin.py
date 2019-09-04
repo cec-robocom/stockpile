104806a8096a9944ddc4afdddc5087730078798c
R-Aravind <aravind.ar786@gmail.com>
2019-09-04 19:29:15 +0530

    registered on admin.py

--- components/admin.py
+++ components/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from components.models import user, component, history
+from components.models import user, component, history, category
 
 admin.site.register(user)
 admin.site.register(component)
+admin.site.register(category)
 admin.site.register(history)
